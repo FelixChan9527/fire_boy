1c5afb4a500d3e6bc1509971d32aad6d90c62b00
5658www638 <2223485532@qq.com>
2021-12-15 09:21:05 +0800

    加注释

--- ball_boy/keras-yolo3-master/yolo3/model.py
+++ ball_boy/keras-yolo3-master/yolo3/model.py
@@ -68,7 +68,9 @@ def make_last_layers(x, num_filters, out_filters):
 
 def yolo_body(inputs, num_anchors, num_classes):
     """Create YOLO_V3 model CNN body in Keras."""
-    darknet = Model(inputs, darknet_body(inputs))
+    darknet = Model(inputs, darknet_body(inputs))   # backbone
+
+    # 下面是关于检测部分的结构，三个
     x, y1 = make_last_layers(darknet.output, 512, num_anchors*(num_classes+5))
 
     x = compose(
@@ -120,22 +122,27 @@ def tiny_yolo_body(inputs, num_anchors, num_classes):
 
 def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     """Convert final layer features to bounding box parameters."""
-    num_anchors = len(anchors)
+    num_anchors = len(anchors)      # num_anchors应该是3，因为每个尺度有3个anchor box
     # Reshape to batch, height, width, num_anchors, box_params.
     anchors_tensor = K.reshape(K.constant(anchors), [1, 1, 1, num_anchors, 2])
 
-    grid_shape = K.shape(feats)[1:3] # height, width
+    # 获得三个尺度的输出大小，分别为(13*13)、(26*26)、(52*52)
+    grid_shape = K.shape(feats)[1:3] # height, width    
     grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
         [1, grid_shape[1], 1, 1])
     grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
-        [grid_shape[0], 1, 1, 1])
+        [grid_shape[0], 1, 1, 1])       # 获得网格的坐标矩阵
     grid = K.concatenate([grid_x, grid_y])
-    grid = K.cast(grid, K.dtype(feats))
+    grid = K.cast(grid, K.dtype(feats))  # 获得网格的坐标矩阵
 
     feats = K.reshape(
         feats, [-1, grid_shape[0], grid_shape[1], num_anchors, num_classes + 5])
 
     # Adjust preditions to each spatial grid point and anchor size.
+    # 此处tx、ty经过了sigmoid函数，这里(K.sigmoid(feats[..., :2]) + grid)
+    # 计算出的是网格大小为1的绝对坐标，需要进行归一化
+    # 宽高也需要归一化
+    # 除了宽高以外，都需要经过sigmoid函数
     box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
     box_wh = K.exp(feats[..., 2:4]) * anchors_tensor / K.cast(input_shape[::-1], K.dtype(feats))
     box_confidence = K.sigmoid(feats[..., 4:5])
@@ -230,6 +237,7 @@ def yolo_eval(yolo_outputs,
 
 
 def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
+    # 这部分代码用于将真实的标签变成与预测相同的格式
     '''Preprocess true boxes to training input format
 
     Parameters
@@ -250,32 +258,47 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
     anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]
 
     true_boxes = np.array(true_boxes, dtype='float32')
+    # input_shape = (416, 416)
     input_shape = np.array(input_shape, dtype='int32')
     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
     boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]
-    true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]
-    true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]
+    true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]   # 归一化
+    true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]   # 归一化
 
-    m = true_boxes.shape[0]
-    grid_shapes = [input_shape//{0:32, 1:16, 2:8}[l] for l in range(num_layers)]
+    m = true_boxes.shape[0]     # 获取标签的batch的大小
+    # 即(13, 13)、(26, 26)、(52, 52)
+    grid_shapes = [input_shape//{0:32, 1:16, 2:8}[l] for l in range(num_layers)]    # 网格数目的列表
+    '''
+    # 跟网络预测一样的大小:
+    (num, 13, 13, 3, 25)
+    (num, 26, 26, 3, 25)
+    (num, 52, 52, 3, 25)
+    '''
     y_true = [np.zeros((m,grid_shapes[l][0],grid_shapes[l][1],len(anchor_mask[l]),5+num_classes),
-        dtype='float32') for l in range(num_layers)]
+        dtype='float32') for l in range(num_layers)]    
 
     # Expand dim to apply broadcasting.
+    # 此处的anchor只有宽高，一共9个anchor box
     anchors = np.expand_dims(anchors, 0)
-    anchor_maxes = anchors / 2.
-    anchor_mins = -anchor_maxes
-    valid_mask = boxes_wh[..., 0]>0
+    # 以下为了得到以anchor box中心为原点的左上和右下的坐标
+    anchor_maxes = anchors / 2.     # 右下坐标
+    anchor_mins = -anchor_maxes     # 左上坐标
+    valid_mask = boxes_wh[..., 0]>0 # 这一步为了得到有目标的物体，他的判断标准是宽大于0
 
-    for b in range(m):
+    for b in range(m):  # 遍历所有图像（一个batch）
         # Discard zero rows.
         wh = boxes_wh[b, valid_mask[b]]
         if len(wh)==0: continue
         # Expand dim to apply broadcasting.
         wh = np.expand_dims(wh, -2)
-        box_maxes = wh / 2.
+        box_maxes = wh / 2.     # 和anchor box操作一样
         box_mins = -box_maxes
 
+        '''
+        # 这一步计算iou，是计算每个网格的anchor box和ground true之间
+        # 的iou值，这一步只针对两个box之间的宽高来计算，即默认二者中心
+        # 是重合的。
+        '''
         intersect_mins = np.maximum(box_mins, anchor_mins)
         intersect_maxes = np.minimum(box_maxes, anchor_maxes)
         intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.)
@@ -285,19 +308,45 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes):
         iou = intersect_area / (box_area + anchor_area - intersect_area)
 
         # Find best anchor for each true box
-        best_anchor = np.argmax(iou, axis=-1)
+        # 算出一个gruond true与9个anhcor box之间
+        # 的iou，并取9个中最优的一个
+        # 留意这里的best_anchor是一张照片的所有最优的iou的
+        # 在每个ground true所对应的9个anchor box中的哪一个
+        best_anchor = np.argmax(iou, axis=-1)   
 
-        for t, n in enumerate(best_anchor):
-            for l in range(num_layers):
-                if n in anchor_mask[l]:
+        # enumerate函数用于遍历其中t为best_anchor中的索引，n为对应索引的内容
+        # b是对应的那个标签，t是一张照片中的所有ground true
+        for t, n in enumerate(best_anchor): # 遍历一张照片的所有ground true对应的最优anchor box的位置
+            for l in range(num_layers):     # 遍历三个尺度
+                if n in anchor_mask[l]:     # 如果这个anchor box属于该层则执行以下程序
+                    # true_boxes[b,t,0]是指第b张图片第t个ground true的宽
+                    # grid_shapes[l][1]为第l个尺度的宽
+                    # np.floor是下取整
+                    # true_boxes[b,t,0]和true_boxes[b,t,1]是归一化
+                    # 后的x，y的坐标，可以看作是在原图中的比例。因此乘以grid_shapes
+                    # 即可得到相应位置坐标
                     i = np.floor(true_boxes[b,t,0]*grid_shapes[l][1]).astype('int32')
                     j = np.floor(true_boxes[b,t,1]*grid_shapes[l][0]).astype('int32')
-                    k = anchor_mask[l].index(n)
-                    c = true_boxes[b,t, 4].astype('int32')
+                    # 此处n的范围是0~8，k是分层后最优anchor box的位置
+                    # best_anchor是没有分层的anchor box的位置
+                    # 对最优的iou对应的anchor box位置的置信度置1
+                    k = anchor_mask[l].index(n)     # 提出该层的anchor box的索引
+                    c = true_boxes[b,t, 4].astype('int32')  # 种类
                     y_true[l][b, j, i, k, 0:4] = true_boxes[b,t, 0:4]
-                    y_true[l][b, j, i, k, 4] = 1
-                    y_true[l][b, j, i, k, 5+c] = 1
+                    y_true[l][b, j, i, k, 4] = 1    # objectness
+                    y_true[l][b, j, i, k, 5+c] = 1  # 对应的种类的位置为1，其余为0
 
+    '''
+    # 这里输出的标签的shape为：
+    # 一共3个尺度组成的list，每个尺度有m张照片对应的label，每个尺度的label大小
+    # 代表了图片被分割成不同数目的网格，分别为13*13、26*26、52*52，每个网格有3
+    # 个对应的anchor box，因此一个ground true会有9个anchor box与之对应，每三
+    # 个负责一个尺度，但只有与ground true间的iou最大的anchor box负责这个
+    # ground true的标签。因此只有一个尺度的一个anchor box有值，这个值分别为
+    # x、y、w、h、1、class，其中种类为one-hot编码，其他8个anchor box的值直接为0。
+    # 三个尺度的shape分别为(50, 13, 13, 3, 25)、(50, 26, 26, 3, 25)
+    # (50, 52, 52, 3, 25)。
+    '''
     return y_true
 
 
@@ -358,44 +407,59 @@ def yolo_loss(args, anchors, num_classes, ignore_thresh=.5, print_loss=False):
     loss: tensor, shape=(1,)
 
     '''
-    num_layers = len(anchors)//3 # default setting
-    yolo_outputs = args[:num_layers]
-    y_true = args[num_layers:]
-    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]
-    input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32, K.dtype(y_true[0]))
-    grid_shapes = [K.cast(K.shape(yolo_outputs[l])[1:3], K.dtype(y_true[0])) for l in range(num_layers)]
+    num_layers = len(anchors)//3 # default setting    # 三个输出尺度
+    yolo_outputs = args[:num_layers]    # args是一各列表，把预测输出的三个尺度和真实的标签的三个尺度进行打包
+    y_true = args[num_layers:]          # 换句话说，要计算损失函数，首先要将label整理成和预测输出相同的格式
+    '''
+    # anchor_mask是anchor box的索引，用于调用对应大小的anchor box。anchor box一共有三类，
+    # 每一类对应一个尺度，每一类又有三个不同的anchor box，分别对应三个bounding box的输出
+    '''
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]  
+    input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32, K.dtype(y_true[0]))    # 获得一张图的预测的三个尺度的输出大小并乘以32
+    grid_shapes = [K.cast(K.shape(yolo_outputs[l])[1:3], K.dtype(y_true[0])) for l in range(num_layers)]    # 和上一条类似，不过将所有的图片的预测输出的尺度大小整合成数组的形式
     loss = 0
-    m = K.shape(yolo_outputs[0])[0] # batch size, tensor
-    mf = K.cast(m, K.dtype(yolo_outputs[0]))
+    m = K.shape(yolo_outputs[0])[0] # batch size, tensor  为了得到这个batch的图像数量  
+    mf = K.cast(m, K.dtype(yolo_outputs[0]))    # 改变类型
 
-    for l in range(num_layers):
-        object_mask = y_true[l][..., 4:5]
+    for l in range(num_layers):     # 对三个尺度进行操作
+        object_mask = y_true[l][..., 4:5]   # 即第五个参数，objectness
         true_class_probs = y_true[l][..., 5:]
 
+        # 这一步为了得到网格坐标矩阵，对应尺度的预测值，绝对坐标（归一化）、以及宽高（归一化）
         grid, raw_pred, pred_xy, pred_wh = yolo_head(yolo_outputs[l],
              anchors[anchor_mask[l]], num_classes, input_shape, calc_loss=True)
         pred_box = K.concatenate([pred_xy, pred_wh])
 
         # Darknet raw box to calculate loss.
-        raw_true_xy = y_true[l][..., :2]*grid_shapes[l][::-1] - grid
+        # 此处把真实的x、y、w、h变成tx、ty、tw、th
+        raw_true_xy = y_true[l][..., :2]*grid_shapes[l][::-1] - grid    # 变为相对网格的坐标
         raw_true_wh = K.log(y_true[l][..., 2:4] / anchors[anchor_mask[l]] * input_shape[::-1])
         raw_true_wh = K.switch(object_mask, raw_true_wh, K.zeros_like(raw_true_wh)) # avoid log(0)=-inf
-        box_loss_scale = 2 - y_true[l][...,2:3]*y_true[l][...,3:4]
+        # 2 - w*h，一个权值，用于加大对小的bounding box的损失，此时的w、h不再需要开根号处理
+        box_loss_scale = 2 - y_true[l][...,2:3]*y_true[l][...,3:4]  
 
         # Find ignore mask, iterate over each of batch.
         ignore_mask = tf.TensorArray(K.dtype(y_true[0]), size=1, dynamic_size=True)
-        object_mask_bool = K.cast(object_mask, 'bool')
+        object_mask_bool = K.cast(object_mask, 'bool')      # 转换类型，转为布尔
         def loop_body(b, ignore_mask):
+            # 得到第l个尺度中第b张图片的所有ground true坐标宽高（以归一化），即objectness为1的对应的值
+            # 将一张图片的所有ground true
             true_box = tf.boolean_mask(y_true[l][b,...,0:4], object_mask_bool[b,...,0])
-            iou = box_iou(pred_box[b], true_box)
-            best_iou = K.max(iou, axis=-1)
+            # 预测的所有bounding box与一张图的所有ground true计算iou
+            iou = box_iou(pred_box[b], true_box)    
+            # 每个bounding box在所有ground true中最大的一个iou，此处的iou
+            best_iou = K.max(iou, axis=-1)     
+            # 选出存在ground true而且iou最优但iou低于阈值的网格位置
             ignore_mask = ignore_mask.write(b, K.cast(best_iou<ignore_thresh, K.dtype(true_box)))
             return b+1, ignore_mask
+        # 遍历图像
         _, ignore_mask = K.control_flow_ops.while_loop(lambda b,*args: b<m, loop_body, [0, ignore_mask])
         ignore_mask = ignore_mask.stack()
         ignore_mask = K.expand_dims(ignore_mask, -1)
 
         # K.binary_crossentropy is helpful to avoid exp overflow.
+        # x、y、w、h的损失值是基于tx、ty、tw、th来计算的
+        # 乘以ignore_mask是为了得到存在物体的网格
         xy_loss = object_mask * box_loss_scale * K.binary_crossentropy(raw_true_xy, raw_pred[...,0:2], from_logits=True)
         wh_loss = object_mask * box_loss_scale * 0.5 * K.square(raw_true_wh-raw_pred[...,2:4])
         confidence_loss = object_mask * K.binary_crossentropy(object_mask, raw_pred[...,4:5], from_logits=True)+ \
